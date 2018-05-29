52518175c5ac43c0924b8cecdb63609d8f621b4a
shamanyu <shubhamshamanyu@gmail.com>
2018-05-29 11:40:16 +0530

    Add an element rendered using react.js and also add style sheet for both elements

--- react-demo/script.js
+++ react-demo/script.js
@@ -7,3 +7,15 @@ jsContainer.innerHTML = `
     Hello JS
   </div>
 `;
+
+const reactContainer = document.getElementById("react");
+
+ReactDOM.render(
+  // React works with objects rather than strings
+  React.createElement(
+    "div",
+    {className: "demo"},
+    "Hello React"
+  ),
+  reactContainer
+);
