e49efab57005c659d40996adcb453457e826e975
shamanyu <shubhamshamanyu@gmail.com>
2018-05-29 11:46:34 +0530

    Make the timestamp update every second for both elements

--- react-demo/script.js
+++ react-demo/script.js
@@ -1,31 +1,36 @@
 console.log("React demo");
 
 const jsContainer = document.getElementById("js");
-
-jsContainer.innerHTML = `
-  <div class="demo">
-    Hello JS
-    <input />
-    <p>
-      ${new Date()}
-    </p>
-  </div>
-`;
-
 const reactContainer = document.getElementById("react");
 
-ReactDOM.render(
-  // React works with objects rather than strings
-  React.createElement(
-    "div",
-    {className: "demo"},
-    "Hello React",
-    React.createElement("input"),
+const render = () => {
+
+  jsContainer.innerHTML = `
+    <div class="demo">
+      Hello JS
+      <input />
+      <p>
+        ${new Date()}
+      </p>
+    </div>
+  `;
+
+  ReactDOM.render(
+    // React works with objects rather than strings
     React.createElement(
-      "p",
-      null,
-      new Date().toString()
-    )
-  ),
-  reactContainer
-);
+      "div",
+      {className: "demo"},
+      "Hello React",
+      React.createElement("input"),
+      React.createElement(
+        "p",
+        null,
+        new Date().toString()
+      )
+    ),
+    reactContainer
+  );
+
+}
+
+setInterval(render, 1000);
